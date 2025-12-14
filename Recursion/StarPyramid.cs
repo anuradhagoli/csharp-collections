b1a63f4a85ac5fb39edcb03c26e79f8dbbbc8f6e
Anuradha Goli <anuradha.yeluri@gmail.com>
2025-12-14 09:48:30 +0000

    Recursion example

---
+++ Recursion/StarPyramid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeSignal.Recursion
+{
+    internal class StarPyramid
+    {
+        public void PrintPyramid(int n )
+        {
+            if (n == 0) return;
+            PrintPyramid(n - 1);
+            for (int i = 1; i <= n; i++)
+            {               
+                Console.Write("*");              
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
